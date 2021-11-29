a31a6a62061bdf7d1c7e3aa8b9a6415ba49ecb05
calabanprivate <alaeff@gmail.com>
2021-11-29 14:43:47 +0300

    init

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,35 +1,38 @@
-import React from 'react';
 import './Map.css';
 import Marker, { IMarker } from './Marker';
 
 const DG = require('2gis-maps');
 
-interface IMap {
+interface IBaseReact {
+    children: any[]    
+}
+
+interface IMap extends IBaseReact {
     center: [number, number],
-    zoom: number
+    zoom: number,
 }
 
-export default class Map extends React.Component<IMap> {
-    map: any; // DG.map result
-    
-    render() {
-        return (<div id="map"
-            ref={(node) => {
-                if (node) {
-                    if (!this.map) {
-                        this.map = DG.map(node, {
-                            'center': this.props.center,
-                            'zoom': this.props.zoom,
-                        });
+function Map(props: IMap) {
+    let map: any; // DG.map result
+
+    return (<div id="map"
+        ref={(node) => {
+            if (node) {
+                if (!map) {
+                    map = DG.map(node, {
+                        'center': props.center,
+                        'zoom': props.zoom,
+                    });
+                }
+
+                (props as any).children.forEach((child: any) => {
+                    if (child.type === Marker) { // т.е. type для компонент - это ф-ия (класс)
+                        DG.marker((child.props as IMarker).coords).addTo(map);
                     }
-
-                    (this.props.children as any).forEach((child: any) => {
-                        if (child.type === Marker) { // т.е. type для компонент - это ф-ия (класс)
-                            DG.marker((child.props as IMarker).coords).addTo(this.map);
-                        }
-                    })
-                };
-            }}
-        />);
-    }
+                })
+            };
+        }}
+    />);
 }
+
+export default Map;
